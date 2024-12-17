0b3b5557aac91e29cdcd61baf0d8738d80d64282
SergeyL-1979 <videomc1979@gmail.com>
2024-12-17 23:00:42 +0300

    обновил views отображения кода карты

--- webbolid/views.py
+++ webbolid/views.py
@@ -1,14 +1,14 @@
 import base64
 
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.db.models import Count, Sum, Case, When, F, ExpressionWrapper, fields
+from django.db.models import Count, Q, Sum, Case, When, F, ExpressionWrapper, fields
 from django.views import generic
 from django_filters.views import FilterView
 from django.db.models.functions import TruncDay, TruncMonth
 
 from webbolid.filters import PLogDataFilter, PlistFilter, TimeFilter
 from webbolid.forms import PlistForm
-from webbolid.models import Plist, Plogdata, Pmark
+from webbolid.models import Plist, Plogdata, Pmark, Groups, Pcompany
 
 
 # ======= Добавлен код для поиска на странице по имени ==================
@@ -107,45 +107,82 @@ class SearchListView(generic.ListView):
 
 
 # ======== ввывод кода карты ========
-class PMarkView(generic.ListView):
+class PMarkSearchByNameView(generic.ListView):
     model = Pmark
     context_object_name = 'codes'
     template_name = 'webbolid/encode_code.html'
-    paginate_by = 5
+    paginate_by = 10
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        """
+        Фильтрация данных на основе GET-параметров запроса.
+        """
+        queryset = Pmark.objects.select_related('owner', 'groupid')  # Используем select_related для оптимизации
+        # Получаем параметры фильтрации из запроса
+        tab_number = self.request.GET.get('tab_number')  # Табельный номер
+        name = self.request.GET.get('name')  # Имя
+        surname = self.request.GET.get('surname')  # Фамилия
+        midname = self.request.GET.get('midname')  # Отчество
+        gtype = self.request.GET.get('gtype')  # Тип пропуска
+        group_id = self.request.GET.get('group_id')  # Группа
+        company_id = self.request.GET.get('company_id')  # Компания
+        status = self.request.GET.get('status')  # Статус карты
 
-        # Обработка каждого объекта и конвертация кода
+        # Добавляем условия фильтрации
+        q_filters = Q()
+        if tab_number:
+            q_filters &= Q(owner__tabnumber__icontains=tab_number)
+        if name:
+            q_filters &= Q(owner__firstname__icontains=name)
+        if surname:
+            q_filters &= Q(owner__name__icontains=surname)
+        if midname:
+            q_filters &= Q(owner__midname__icontains=midname)
+        if gtype:
+            q_filters &= Q(gtype=gtype)
+        if group_id:
+            q_filters &= Q(groupid__id=group_id)
+        if company_id:
+            q_filters &= Q(owner__company__id=company_id)
+        if status:
+            q_filters &= Q(status=status)
+        # return queryset.filter(q_filters)
+        # Фильтруем данные
+        queryset = queryset.filter(q_filters)
+        # Конвертация кода для каждого объекта
         for obj in queryset:
-            raw_value = obj.codep
+            raw_value = obj.codep  # Исходное значение кода карты
 
             try:
                 # Конвертируем строку в байты с кодировкой cp1251
                 byte_value = raw_value.encode('Windows-1251', errors='ignore')
-                # byte_value = raw_value.encode('cp1251', errors='ignore')
                 # Отбрасываем первый байт
                 modified_bytes = byte_value[1:]
                 # Заменяем пары байт FE01 на 00
                 modified_bytes = modified_bytes.replace(b'\xFE\x01', b'\x00')
                 # Переворачиваем байты
                 reversed_bytes = modified_bytes[::-1]
+                # Заменяем пары байт 03FE на 20
                 reversed_bytes = reversed_bytes.replace(b'\x03\xFE', b'\x20')
                 # Преобразуем в шестнадцатеричное представление
                 hex_representation = reversed_bytes.hex().upper()
+                # Сохраняем преобразованный код в объекте
+                obj.converted_code = hex_representation
 
-            except Exception as e:
-                hex_representation = f"Ошибка преобразования: {str(e)}"
-
-            # Сохраняем преобразованное значение в объекте
-            obj.converted_code = hex_representation
+            except UnicodeEncodeError as e:
+                print(f"Encoding error for {obj.id}: {e}")
+                obj.converted_code = None  # В случае ошибки сохраняем None
 
         return queryset
 
     def get_context_data(self, **kwargs):
+        """
+        Добавляем дополнительные данные для отображения в шаблоне.
+        """
         context = super().get_context_data(**kwargs)
-        # Передаем список с преобразованными кодами в контекст
-        context['codes'] = self.get_queryset()
+        context['groups'] = Groups.objects.all()  # Список групп для фильтрации
+        context['companies'] = Pcompany.objects.all()  # Список компаний
+        context['filters'] = self.request.GET  # Передаём текущие фильтры для шаблона
         return context
 
 
